3e5355d5c4d4bbf4c966c2e4a6d3e5cc26495265
Dakota Smith <dssmith43@students.tntech.edu>
2016-09-02 20:06:55 -0500

    Enabled all functions for testing

--- Lab02Driver.cpp
+++ Lab02Driver.cpp
@@ -1,5 +1,5 @@
 #include "ReadFile.h"//I modified this one. - D. Smith
-#include "WriteFile.h"
+#include "WriteFile.h"//slcaneer42
 #include "String.h"//Not modified
 #include "Tokens.h"//Not sure why this is included at this time
 
@@ -14,19 +14,19 @@
 int main()
 {
    ReadFile* rf = new ReadFile("cds.txt"); //Use of the constructor for ReadFile. Needs new keyword in driver.
-   //WriteFile* wf = new WriteFile("out.txt");
+   WriteFile* wf = new WriteFile("out.txt"); //Use of the constructor for WriteFile. Again a new keyword.
 
    while(!rf->eof())//Loop that runs until the end of the file.
    {
       String* line = rf->readLine();//Calling the readLine function from the ReadFile class to output a String* type variable
-      //wf->writeLine(line);
+      wf->writeLine(line);//Calling the writeFile function to output to a file.
       delete line;//Deletes the String* variable.
    }
 
    rf->close();//ReadFile class variable calls the close function to close the file and set closed to true
-   //wf->close();
+   wf->close();//Closes the file being written to.
    delete rf;//Delete the ReadFile* variable in order to save space.
-   //delete wf;
+   delete wf;
 
    return 0;
 }
